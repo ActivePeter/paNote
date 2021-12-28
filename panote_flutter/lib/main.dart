d822edd1bbe4e83a1c335cd3a4b4a00618e15fa2
ActivePeter <1020401660@qq.com>
2021-12-28 13:41:51 +0800

    s

--- panote_flutter/lib/main.dart
+++ panote_flutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart' as v;
 
 void main() {
   runApp(const MyApp());
@@ -62,6 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
   double scale_dx = 0, scale_dy = 0;
   bool dragging = false;
   Matrix4 matrix = Matrix4.identity();
+  FocusNode _focusNode = FocusNode();
 
   void _incrementCounter() {
     setState(() {
@@ -74,11 +76,85 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Container main_container() {
+    return Container(
+        color: Colors.red,
+        constraints: BoxConstraints(
+            minWidth: double.infinity, minHeight: double.infinity),
+        child: Stack(
+          children: [
+            Transform(
+              transform: matrix
+              //matrix,
+              // Matrix4.identity()
+              //   ..translate(map_dx, map_dy)
+              //   ..multiply(Matrix4.identity()
+              //     // ..r
+              //     // ..translate(scale_dx, scale_dy)
+              //     ..scale(scale))
+              ,
+              // ..translate(-scale_dx, -scale_dy)),
+              child: Container(
+                  width: 500,
+                  height: 500,
+                  color: Colors.blue,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          left: dx,
+                          top: dy,
+                          child: Draggable(
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              color: Colors.yellow,
+                            ),
+                            feedback: Text('data'),
+                            onDragEnd: (s) {
+                              if (!scaling) {
+                                var v1 =
+                                    v.Vector4(s.offset.dx, s.offset.dy, 0, 0);
+                                print(v1);
+                                print(matrix);
+
+                                var m = Matrix4.inverted(matrix);
+                                print(m);
+                                var a = m.transform(
+                                    v.Vector4(s.offset.dx, s.offset.dy, 0, 0));
+
+                                print(a);
+
+                                setState(() {
+                                  dx = m.row0[0] * v1.x + m.row0[3];
+                                  print(m.row1);
+                                  dy = m.row1[1] * v1.y + m.row1[3];
+                                  print(dx);
+                                  print(dy);
+                                  print(" ");
+                                });
+                              }
+                            },
+                          ))
+                    ],
+                  )),
+            ),
+          ],
+        ));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _focusNode.requestFocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
+
     // Initialize a stream for the KeyEvents:
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
@@ -89,88 +165,103 @@ class _MyHomePageState extends State<MyHomePage> {
       //   // the App.build method, and use it to set our appbar title.
       //   title: Text(widget.title),
       // ),
-      body: Listener(
-          onPointerSignal: (s) {
-            if (s is PointerScrollEvent) {
-              var scale2 = scale;
-              print("object $s.scrollDelta");
-              scale2 += s.scrollDelta.dy / 150;
-              if (scale2 > 1.1) {
-                scale2 = 1.1;
+      body: MouseRegion(
+        cursor: scaling ? SystemMouseCursors.click : SystemMouseCursors.grab,
+        child: Listener(
+            onPointerSignal: (s) {
+              if (s is PointerScrollEvent) {
+                if (scaling) {
+                  var scale2 = scale;
+                  print("object $s.scrollDelta");
+                  scale2 += s.scrollDelta.dy / 150;
+                  if (scale2 > 1.1) {
+                    scale2 = 1.1;
+                  }
+                  if (scale2 < 0.3) {
+                    scale2 = 0.3;
+                  }
+                  scale_dx = s.localPosition.dx;
+                  scale_dy = s.localPosition.dy;
+
+                  var dscale = scale2 / scale;
+                  scale = scale2;
+
+                  setState(() {
+                    matrix =
+                        // ..multiply(Matrix4.identity()
+                        // ..scale(dscale)
+                        // ..translate()
+                        // );
+                        (Matrix4.identity()..translate(scale_dx, scale_dy))
+                          ..multiply(Matrix4.identity()..scale(dscale))
+                          ..multiply(Matrix4.identity()
+                            ..translate(-scale_dx, -scale_dy))
+                          ..multiply(matrix);
+                    // matrix
+                    //   ..translate(s.localDelta.dx, s.localDelta.dy)
+                    //   ..scale(1.0)
+                    //   ..scale(scale2)
+                    //   ..translate(-s.localDelta.dx, -s.localDelta.dy);
+                    // scale = scale2;
+                  });
+                }
               }
-              if (scale2 < 0.3) {
-                scale2 = 0.3;
+
+              // if (s is PointerDownEvent) {
+              // dragging = s.down;
+              // }
+            },
+            onPointerDown: (s) {
+              if (s.down) {
+                dragging = true;
               }
-              scale_dx = s.localPosition.dx;
-              scale_dy = s.localPosition.dy;
-
-              var dscale = scale2 / scale;
-              scale = scale2;
-
-              setState(() {
-                matrix =
-                    // ..multiply(Matrix4.identity()
-                    // ..scale(dscale)
-                    // ..translate()
-                    // );
-                    (Matrix4.identity()..translate(scale_dx, scale_dy))
-                      ..multiply(Matrix4.identity()..scale(dscale))
-                      ..multiply(
-                          Matrix4.identity()..translate(-scale_dx, -scale_dy))
-                      ..multiply(matrix);
-                // matrix
-                //   ..translate(s.localDelta.dx, s.localDelta.dy)
-                //   ..scale(1.0)
-                //   ..scale(scale2)
-                //   ..translate(-s.localDelta.dx, -s.localDelta.dy);
-                // scale = scale2;
-              });
-            }
-
-            // if (s is PointerDownEvent) {
-            // dragging = s.down;
-            // }
-          },
-          onPointerDown: (s) {
-            if (s.down) {
-              dragging = true;
-            }
-          },
-          onPointerUp: (s) {
-            if (!s.down) {
-              dragging = false;
-            }
-          },
-          onPointerMove: (s) {
-            print('move $s');
-            setState(() {
-              map_dx += s.delta.dx;
-              map_dy += s.delta.dy;
-              matrix.translate(s.delta.dx / scale, s.delta.dy / scale);
-            });
-          },
-          child: Container(
-              color: Colors.red,
-              constraints: BoxConstraints(
-                  minWidth: double.infinity, minHeight: double.infinity),
-              child: Stack(
-                children: [
-                  Transform(
-                      transform: matrix
-                      //matrix,
-                      // Matrix4.identity()
-                      //   ..translate(map_dx, map_dy)
-                      //   ..multiply(Matrix4.identity()
-                      //     // ..r
-                      //     // ..translate(scale_dx, scale_dy)
-                      //     ..scale(scale))
-                      ,
-                      // ..translate(-scale_dx, -scale_dy)),
-                      child: Container(
-                          width: 200, height: 200, color: Colors.blue)),
-                ],
-              ))),
-
+            },
+            onPointerUp: (s) {
+              if (!s.down) {
+                dragging = false;
+              }
+            },
+            onPointerMove: (s) {
+              // print('move $s');
+              if (scaling) {
+                setState(() {
+                  map_dx += s.delta.dx;
+                  map_dy += s.delta.dy;
+                  matrix.translate(s.delta.dx / scale, s.delta.dy / scale);
+                });
+              }
+            },
+            child: RawKeyboardListener(
+              child: main_container(),
+              focusNode: _focusNode,
+              onKey: (e) {
+                setState(() {
+                  // if(e.logicalKey == LogicalKeyboardKey.alt ||
+                  // e.logicalKey == LogicalKeyboardKey.altLeft ||
+                  // e.logicalKey == LogicalKeyboardKey.altRight
+                  // ){
+                  // scaling=e.isKeyPressed();
+                  // }e
+                  scaling = e.isKeyPressed(LogicalKeyboardKey.alt) ||
+                      e.isKeyPressed(LogicalKeyboardKey.altLeft) ||
+                      e.isKeyPressed(LogicalKeyboardKey.altRight);
+                  print(scaling);
+                });
+              },
+            )),
+        // RawKeyboardListener(
+        //   focusNode: FocusNode(),
+        //   onKey: (event) {
+        //     setState(() {
+        //       scaling = event.isKeyPressed(LogicalKeyboardKey.alt) ||
+        //           event.isKeyPressed(LogicalKeyboardKey.altLeft) ||
+        //           event.isKeyPressed(LogicalKeyboardKey.altRight);
+        //       print(scaling);
+        //     });
+        //   },
+        //   child:,
+        // )),
+      ),
       //   Stack(
       //   children: <Widget>[
       //     Transform(
