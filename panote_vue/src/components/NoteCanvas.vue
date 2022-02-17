f713ad27699921e44ff48ef2b90d7f43d5159a6e
ActivePeter <1020401660@qq.com>
2022-02-17 21:50:59 +0800

    add margin

--- panote_vue/src/components/NoteCanvas.vue
+++ panote_vue/src/components/NoteCanvas.vue
@@ -1,39 +1,75 @@
 <template>
   <div class="note_canvas">
-    <div class="range">
-      <div class="content" :style="{ transform: 'scale(' + scale + ')' }">
+    <div
+      class="range"
+      ref="range_ref"
+      @mousedown="handle_mouse_down"
+      @mouseup="handle_mouse_up"
+    >
+      <div
+        class="content"
+        :style="{
+          transform: 'scale(' + scale + ')',
+          margin: edge_size_h + 'px ' + edge_size_w + 'px',
+        }"
+      >
         content
       </div>
     </div>
     <div class="info">
-      scroll_enabled:{{ scroll_enabled }} scale: {{ scale }}
+      scroll_enabled:{{ scroll_enabled }}, scale: {{ scale }}, dragging:
+      {{ dragging }}
     </div>
   </div>
 </template>
 <script>
+import ElementResizeDetectorMaker from "element-resize-detector";
+
 export default {
   name: "NoteCanvas",
   mounted() {
     window.addEventListener("keyup", this.handle_key_up);
     window.addEventListener("keydown", this.handle_key_down);
     window.addEventListener("mousewheel", this.handle_scroll);
+    window.addEventListener("mouseup", this.handle_mouse_up);
+    window.addEventListener("mousemove", this.handle_mouse_move);
+    let erd = ElementResizeDetectorMaker();
+    let _this = this;
+    erd.listenTo(this.$refs.range_ref, function (element) {
+      var width = element.offsetWidth;
+      var height = element.offsetHeight;
+      console.log("Size: " + width + "x" + height);
+      if (width > 100) {
+        _this.edge_size_w = width - 80;
+      } else {
+        _this.edge_size_w = width;
+      }
+      if (height > 100) {
+        _this.edge_size_h = height - 80;
+      } else {
+        _this.edge_size_h = height;
+      }
+    });
   },
   data() {
     return {
       scroll_enabled: false,
       scale: 1,
-      scale_step: 0.01,
+      scale_step: 0.1,
+      dragging: false,
+      edge_size_w: 100,
+      edge_size_h: 100,
     };
   },
   methods: {
     handle_key_up(val) {
-      if (val.key == " ") {
+      if (val.key == "b") {
         console.log("handle_key_up", val);
         this.scroll_enabled = false;
       }
     },
     handle_key_down(val) {
-      if (val.key == " ") {
+      if (val.key == "b") {
         console.log("handle_key_down", val);
         this.scroll_enabled = true;
       }
@@ -43,6 +79,19 @@ export default {
         console.log("handle_scroll", val);
         this.scale_canvas(val.deltaY);
       }
+    },
+    handle_mouse_down() {
+      this.dragging = true;
+    },
+    handle_mouse_move(val) {
+      if (val.buttons == 1 && this.scroll_enabled) {
+        console.log(val);
+      } else {
+        this.dragging = false;
+      }
+    },
+    handle_mouse_up() {
+      this.dragging = false;
     },
     scale_canvas(dir) {
       let scale_bak = this.scale;
@@ -71,12 +120,8 @@ export default {
   overflow: scroll;
 }
 .content {
-  position: relative;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
   background-color: rgb(255, 200, 200);
-  height: 100%;
+  height: 400px;
+  width: 300px;
 }
 </style>
