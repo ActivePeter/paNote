8a1ef61834cf88f6ad0e3f55d43f67ab622746c6
ad56583964 <1176949486@qq.com>
2022-02-18 07:04:53 +0800

    add @editorjs/editorjs

--- panote_vue/src/components/EditorBar.vue
+++ panote_vue/src/components/EditorBar.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="editor_bar">editor_bar</div>
+  <div class="editor_bar">editor_bar
+    <div id="editorjs"></div>
+  </div>
 </template>
 
 <script>
+import EditorJS from '@editorjs/editorjs';
+
 export default {
   name: "EditorBar",
   mounted() {},
@@ -12,6 +16,9 @@ export default {
   methods: {},
   props: {},
 };
+
+const editor = new EditorJS();
+
 </script>
 
 <style scoped>
