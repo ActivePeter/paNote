3dc69bdd40a50d65e70ebef039ebe793cdbfa26c
ActivePeter <1020401660@qq.com>
2022-02-17 23:07:29 +0800

    收

---
+++ panote_vue/src/components/EditorBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="editor_bar"></div>
+</template>
+
+<script>
+export default {
+  name: "EditorBar",
+  mounted() {},
+  data() {
+    return {};
+  },
+  methods: {},
+  props: {},
+};
+</script>
+
+<style scoped>
+</style>
