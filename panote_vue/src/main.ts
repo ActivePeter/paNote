e41d2e13b140fe644128ff2301ab58d44bebd50b
ActivePeter <1020401660@qq.com>
2022-04-17 23:40:13 +0800

    s

--- panote_vue/src/main.ts
+++ panote_vue/src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+
 // import { QuillEditor } from '@vueup/vue-quill'
 // import '@vueup/vue-quill/dist/vue-quill.snow.css';
-
 // global
 // import { quillEditor } from 'vue3-quill'
 // import toolbar from 'toolbar'
