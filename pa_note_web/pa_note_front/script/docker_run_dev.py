e8cc0305258771a716d3d10e7d1f3e9e27e785f8
ActivePeter <1020401660@qq.com>
2024-06-13 18:59:46 +0800

    feat: packs missing, docker dev environment

---
+++ pa_note_web/pa_note_front/script/docker_run_dev.py
@@ -0,0 +1,52 @@
+### chdir
+import os
+import sys
+
+CUR_FPATH = os.path.abspath(__file__)
+CUR_FDIR = os.path.dirname(CUR_FPATH)
+os.chdir("..")
+
+
+### utils
+def os_system_sure(command):
+    print(f"执行命令：{command}")
+    result = os.system(command)
+    if result != 0:
+        print(f"命令执行失败：{command}")
+        exit(1)
+    print(f"命令执行成功：{command}\n\n")
+
+def os_system(command):
+    print(f"执行命令：{command}")
+    result = os.system(command)
+    print("\n\n")
+
+
+
+ENTRY="""
+pnpm install --save clone deep-equal eventemitter3 extend parchment quill-delta@3.6.0 tslib
+pnpm install
+pnpm run serve
+"""
+
+DOCKER_COMPOSE="""
+version: '3'
+services:
+  panote_ui_dev:
+    image: panote_ui_dev
+    # 使用 host 网络模式
+    network_mode: host
+    # 挂载项目目录到容器中
+    volumes:
+      - .:/usr/src/app
+    command: bash docker_run_dev_entry.sh
+"""
+
+with open("docker_run_dev_entry.sh", "w") as f:
+    f.write(ENTRY)
+
+with open("docker-compose.yml", "w") as f:
+    f.write(DOCKER_COMPOSE)
+
+os_system_sure("docker-compose up")
+
